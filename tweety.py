841f83a134ba1e0feaf58293c914580ca3b0df6f
Preeti Singh <preetisingh91997@gmail.com>
2018-01-02 09:28:49 -0800

    Enhanced version of tweety

--- tweety.py
+++ tweety.py
@@ -1,17 +1,23 @@
 import requests
+from termcolor import colored
+
 from bs4 import BeautifulSoup
 
 req = requests.get('https://www.twitter.com/')
 print(req)
 page = req.content
-
 soup = BeautifulSoup(page,'html.parser')
 
-mera_moto = soup.findAll('p', {'class': 'tweet-text'})
-    
-for moto in mera_moto:
-    print('----------------------------------------------------------------------')
-    print(moto.text)    
-     
+twitter_account = soup.findAll('div', {'class': 'content'})
+
+line=0
+for tweet in twitter_account:
+    print(colored ('-'*73,"yellow"))
+    line = line + 1
+    print(colored(tweet.strong.text,"red"))
+    if line%2==0:
+        print(tweet.p.text)
+    else:
+        print(colored(tweet.p.text,"blue"))
 hello = input()
 print(hello)
